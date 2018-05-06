56277fa850502bb4eb86668912dfbfd2d856ee4a
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-05-06 23:29:04 +0200

    * wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+
+
+fn new_block(){
+
+}
+
+
+fn hash(){
+
+}
+
+pub struct Transaction {
+    amount: u32,
+    recipient: String,
+    sender: String
+}
+
+pub struct Blockchain {
+    chain: Vec<Block>,
+    current_transactions: Vec<Transaction>
+}
+
+pub struct Block {
+    index: u32,
+    previous_hash: String,
+    proof: String,
+    timestamp: u32,
+    transactions: Vec<Transaction>
+}
+
+impl Blockchain {
+    pub fn new_transaction(&self, transaction: &Transaction){
+
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let vec: Vec<Block> = Vec::new();
+    let trans: Vec<Transaction> = Vec::new();
+    let chain =  Blockchain { chain: vec, current_transactions: trans};
+    let transaction = Transaction { amount: 5, recipient: "me".to_string(), sender: "you".to_string()};
+    chain.new_transaction(&transaction);
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_transaction_is_added() {
+        let chain: Blockchain;
+        let transaction = Transaction { amount: 5, recipient: "me".to_string(), sender: "you".to_string()};
+        chain.new_transaction(&transaction);
+        assert!(0, 1);
+    }
 }
