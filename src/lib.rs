7c88fa1d6b13ece7e9dcdfc17b86a0638eaeb43b
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-05-14 20:18:15 +0200

    * first test passes

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+
+fn new_block(){
+
+}
+
+
+fn hash(){
+
+}
+
+#[derive(PartialEq, PartialOrd)]
+pub struct Transaction {
+    amount: u32,
+    recipient: String,
+    sender: String
+}
+
+
+pub struct Block {
+    index: u32,
+    previous_hash: String,
+    proof: String,
+    timestamp: u32,
+    transactions: Vec<Transaction>
+}
+
+pub struct Blockchain {
+    chain: Vec<Block>,
+    current_transactions: Vec<Transaction>
+}
+
+impl Blockchain {
+    pub fn new_transaction(&mut self, transaction: Transaction){
+        self.current_transactions.push(transaction);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    use super::*;
+
+    #[test]
+    fn new_transaction_is_added() {
+        let vec: Vec<Block> = Vec::new();
+        let trans: Vec<Transaction> = Vec::new();
+        let mut chain = Blockchain { chain: vec, current_transactions: trans};
+        let transaction = Transaction { amount: 5, recipient: "me".to_string(), sender: "you".to_string() };
+        chain.new_transaction(transaction);
+        assert_eq!(1, chain.current_transactions.len());
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,6 @@
 
 
-fn new_block(){
-
-}
-
-
-fn hash(){
-
-}
-
-pub struct Transaction {
-    amount: u32,
-    recipient: String,
-    sender: String
-}
-
-pub struct Blockchain {
-    chain: Vec<Block>,
-    current_transactions: Vec<Transaction>
-}
-
-pub struct Block {
-    index: u32,
-    previous_hash: String,
-    proof: String,
-    timestamp: u32,
-    transactions: Vec<Transaction>
-}
-
-impl Blockchain {
-    pub fn new_transaction(&self, transaction: &Transaction){
-
-    }
-}
-
 fn main() {
 
-
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn new_transaction_is_added() {
-        let vec: Vec<Block> = Vec::new();
-        let trans: Vec<Transaction> = Vec::new();
-        let chain =  Blockchain { chain: vec, current_transactions: trans};
-        let transaction = Transaction { amount: 5, recipient: "me".to_string(), sender: "you".to_string()};
-        chain.new_transaction(&transaction);
-        assert!(true, true);
-    }
+    println!("DUPA");
 }
