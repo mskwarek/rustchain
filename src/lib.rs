dfc31cd7b05ea4e8459a01c0b946dcc26ef2edda
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-05-14 21:17:47 +0200

    * unit test for index incrementing prepared

--- src/lib.rs
+++ src/lib.rs
@@ -27,13 +27,14 @@ pub struct Blockchain {
 }
 
 impl Blockchain {
-    pub fn new_transaction(&mut self, transaction: Transaction) -> i32{
+    pub fn new_transaction(&mut self, transaction: Transaction) -> u32{
         self.current_transactions.push(transaction);
-        match self.chain.last()
+        let num = match self.chain.last()
             {
-                Some(_) => 1,
+                Some(_) => self.chain.last().unwrap().index,
                 None => 0
-            }
+            };
+        num+1
     }
 }
 
@@ -46,15 +47,32 @@ mod tests {
 
     use super::*;
 
+
     #[test]
     fn new_transaction_is_added() {
         let vec: Vec<Block> = Vec::new();
         let trans: Vec<Transaction> = Vec::new();
         let mut chain = Blockchain { chain: vec, current_transactions: trans};
+
         let transaction = Transaction { amount: 5, recipient: "me".to_string(), sender: "you".to_string() };
         let index = chain.new_transaction(transaction);
         assert_eq!(1, chain.current_transactions.len());
-        assert_eq!(index, 0);
+        assert_eq!(index, 1);
+    }
+
+    #[test]
+    fn last_index_is_incremented() {
+        let vec: Vec<Block> = Vec::new();
+        let trans: Vec<Transaction> = Vec::new();
+        let mut chain = Blockchain { chain: vec, current_transactions: trans};
+
+        let transaction = Transaction { amount: 5, recipient: "me".to_string(), sender: "you".to_string() };
+        let index = chain.new_transaction(transaction);
+        assert_eq!(1, chain.current_transactions.len());
+        let index2 = chain.new_transaction(Transaction { amount: 10, recipient: "you".to_string(), sender: "me".to_string() });
+        assert_eq!(index, 1);
+        assert_eq!(2, chain.current_transactions.len());
+        assert_eq!(index2, 2);
     }
 
 }
