117b1aefc3eb8dd418af842287e0148363b1c7b8
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-05-17 19:42:40 +0200

    * added reward for mining

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,12 @@ extern crate crypto;
 extern crate serde_derive;
 extern crate serde_json;
 extern crate rustc_serialize;
+extern crate uuid;
 
 use rustc_serialize::hex::ToHex;
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
+use uuid::Uuid;
 
 #[derive(PartialEq, PartialOrd)]
 #[derive(Clone)]
@@ -30,7 +32,8 @@ pub struct Block {
 
 pub struct Blockchain {
     chain: Vec<Block>,
-    current_transactions: Vec<Transaction>
+    current_transactions: Vec<Transaction>,
+    node_identifier: String
 }
 
 impl Blockchain {
@@ -38,7 +41,8 @@ impl Blockchain {
         let mut vec: Vec<Block> = Vec::new();
         vec.push(Block { index: 1, previous_hash: "1".to_string(), proof: 100, timestamp: 333037375,
             transactions: Vec::new() });
-        Blockchain { chain: vec, current_transactions: Vec::new() }
+        let my_uuid = Uuid::new_v4();
+        Blockchain { chain: vec, current_transactions: Vec::new(), node_identifier: my_uuid.to_string().replace("-", "") }
     }
     pub fn new_transaction(&mut self, transaction: Transaction) -> u32 {
         self.current_transactions.push(transaction);
@@ -91,10 +95,15 @@ impl Blockchain {
         proof
     }
 
+    fn reward_for_proof(&mut self) {
+        self.current_transactions.push( Transaction {amount: 1, recipient: self.node_identifier.clone(), sender: "0".to_string()} );
+    }
+
     pub fn mine_new_block(&mut self) -> Block {
         let last_block = self.chain.last().unwrap().clone();
         let proof = Blockchain::proof_of_work(last_block.proof);
         let previous_hash = Blockchain::calculate_hash_from_block(&last_block);
+        self.reward_for_proof();
         self.new_block(proof, previous_hash)
     }
 }
@@ -160,7 +169,7 @@ mod tests {
         let transaction = Transaction { amount: 5, recipient: "me".to_string(), sender: "you".to_string() };
         let index = chain.new_transaction(transaction);
         assert_eq!(1, chain.current_transactions.len());
-        let block = chain.mine_new_block();
+        chain.mine_new_block();
         let index2 = chain.new_transaction(Transaction { amount: 10, recipient: "you".to_string(), sender: "me".to_string() });
         assert_eq!(1, chain.current_transactions.len());
         assert_eq!(index, 2);
